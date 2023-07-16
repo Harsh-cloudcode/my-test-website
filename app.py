88b1c9115aed03e620ff2f18f1d2daa1ed17b76f
Harsh-cloudcode <136334910+Harsh-cloudcode@users.noreply.github.com>
2023-07-16 08:45:43 +0000

    Web structure

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analytics',
+    'location': 'Pune, India',
+    'salary': 'Rs. 1,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Banglore, India'
+  },
+  {
+    'id': 3,
+    'title': 'Web Developer',
+    'location': 'delhi, India'
+  },
+  {
+    'id': 4,
+    'title': 'PHP developer',
+    'location': 'Mumbai, India',
+    'salary': 'Rs. 3,00,000'
+  },
+]
 
 
 @app.route("/")
 def hello_harshal():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, page_name='Sample')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
